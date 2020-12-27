96f5fff9267b1bca1882535ef5865241abf6bd87
gissandrogama <gissandrogama@gmail.com>
2020-12-27 15:29:14 -0300

    create module assinante, telefonia

---
+++ lib/telefonia.ex
@@ -0,0 +1,14 @@
+defmodule Telefonia do
+  @moduledoc """
+  Documentation for `Telefonia`.
+  """
+
+  def start do
+    File.write!("pre.txt", :erlang.term_to_binary([]))
+    File.write!("pos.txt", :erlang.term_to_binary([]))
+  end
+
+  def cadastrar_assinante(nome, numero, cpf, plano) do
+    Assinante.cadastrar(nome, numero, cpf, plano)
+  end
+end
